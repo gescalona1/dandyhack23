fd63dba7adf3ac1efc18185ceb31037e2b0b0615
giordan <giordane02@gmail.com>
2023-11-04 13:34:51 -0400

    added profile page

---
+++ components/client.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useUser } from '@auth0/nextjs-auth0/client';
+
+const ProfileClient = () => {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+  console.log(user);
+  return (
+    
+    user && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+}
+
+export default ProfileClient;
---
+++ src/app/profile/page.js
@@ -0,0 +1,9 @@
+import ProfileClient from "../../../components/client"
+
+export default function ProfilePage() {
+    return (
+        <>
+        <ProfileClient />
+        </>
+    )
+}
